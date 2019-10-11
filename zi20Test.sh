6e061b8d71485e9db4b6816cbacfb097b487180f
Nub5ecCo <55526852+Nub5ecCo@users.noreply.github.com>
2019-10-11 15:15:00 +0700

    Add files via upload

---
+++ zi20Test.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#__________________________
+#|        _________   |       |         |   
+#|     /______     /    |     |          |
+#|               /   /       |     |          |
+#|            /   /          |     |          |
+#|          /   /_____   |     |          |
+#|____ /________/_|____|____|
+#SPAM KONTOL FOR AAY:v PERCOBAAN AGAR LITESPAM TOKED SINGKAT
+echo "close"
+print "ZI20 PROJECT"
+}
+load(){
+    echo -e "\n"
+    bar=" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "
+    barlength=${#bar}
+    i=0
+    while((i<100)); do
+        n=$((i*barlength / 100))
+        printf "\e[00;32m\r[%-${barlength}s]\e[00m" "${bar:0:n}"
+        ((i += RANDOM%5+2))
+        sleep 0.2
+    done
+0.03
+0.7
+0.2
+echo "LOADING."
+  0.1
+   echo "LOADING.."
+     0.1
+      echo "LOADING..."
+        0.1
+         echo "LOADING...."
+           0.1
+            echo "LOADING....."
+              0.1
+               echo "LOADING......"     
+print "LAMA BANGSAD 1!1!1!"
+     sleep 0.2
+     echo "toilet -f mono12 -F gay KONTOL OKE | lolcat"
+     sleep 2
+     echo "git clone https://github.com/4L13199/LITESPAM"
+     sleep 2
+     echo "cd LITESPAM"
+     sleep 2
+     echo "sh LITESPAM.sh
+      0.1
+     echo "4"
+     0.1
+     echo "089666616200"
+     
+     echo "clear"
+     
+     echo "sh LITESPAM.sh"
+     
+     echo "1"
+     sleep 0.2
+     echo "089666616200"
+     
+     echo "y"
+     
+     echo "100"
+     echo "clear"
+     echo "toilet -f mono12-F gay TQ FOR | lolcat"
+     echo "figlet ZI20 | lolcat"
